f4281dc2988c94f16226262ee93f5e9e44d69074
Arjan Scherpenisse <arjan@scherpenisse.net>
2020-06-04 22:19:39 +0200

    Some more test cases for protocols

--- test/bubble_match_test.exs
+++ test/bubble_match_test.exs
@@ -16,16 +16,22 @@ defmodule BubbleMatchTest do
   end
 
   test "can be inspected" do
+    assert "#BML<>" == inspect(%BubbleMatch{})
+
     bml = BubbleMatch.parse!("hello world")
     assert "#BML<hello world>" == inspect(bml)
   end
 
   test "can be converted to string" do
+    assert "" == to_string(%BubbleMatch{})
+
     bml = BubbleMatch.parse!("hello world")
     assert "hello world" == to_string(bml)
   end
 
   test "can encode to JSON" do
+    assert "\"\"" == Jason.encode!(%BubbleMatch{})
+
     bml = BubbleMatch.parse!("hello world")
     assert "\"hello world\"" == Jason.encode!(bml)
   end
